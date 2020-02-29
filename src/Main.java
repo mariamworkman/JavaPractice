56e75eb5cd43f65de3c35c5f1962cd3a2fcc2136
mariamworkman <60803434+mariamworkman@users.noreply.github.com>
2020-02-28 18:47:45 -0800

    palindrome

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner (System.in);
-        String word;
-        word = scanner.nextLine();
 
-        if ((word.length()/2)*2 == word.length()){
-            System.out.println("String is even!");
-        }else{
-            System.out.println("String is not even!");
+        String word, reverse = "";
+        word = scanner.nextLine();
+        int length = word.length();
+        for (int i = length -1; i >=0; i--)
+            reverse = reverse + word.charAt(i);
+        if (word.equals(reverse)){
+            System.out.println("This string is a palindrome");
+        } else {
+            System.out.println("This string is not a palindrome");
         }
     }
 }
